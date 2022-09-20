2b2acf7e923481037b44bb0fcca77eff1637c708
Ravind Raniga <ravindraniga@elara.ai>
2022-09-20 16:35:51 +1000

    fix: post build for 03_launch_a_solution

---
+++ 00_platform_essentials/02_get_data_in_and_out/03_launch_a_solution/01/gen/my_datastream.template.ts
@@ -0,0 +1,16 @@
+// GENERATED FILE, DO NOT EDIT
+export default {
+datasources: {},
+pipelines: {},
+models: {},
+environments: {},
+tables: {"My Datastream":{
+kind:"stream" as const,
+name:"My Datastream",
+type:{
+type:'Integer' as const,value:null},
+},
+},
+scenarios: {},
+}
+// GENERATED FILE, DO NOT EDIT
--- 00_platform_essentials/02_get_data_in_and_out/03_launch_a_solution/01_post_build/gen/my_datastream.stream.ts
+++
@@ -1,9 +0,0 @@
-// GENERATED FILE, DO NOT EDIT
-export default {
-kind:"stream" as const,
-name:"My Datastream",
-type:{
-type:'Integer' as const,
-value:null},
-}
-// GENERATED FILE, DO NOT EDIT
