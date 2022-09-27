556c4a5956c472bf6b87cc61c41e47d9ce3b7152
Ravind Raniga <ravindraniga@elara.ai>
2022-09-27 12:38:08 +1000

    fix: update to pipeline stackblitz project for transforming data

--- 00_platform_essentials/03_transform_data/02_define_and_launch_a_pipeline/00/src/my_pipelines.ts
+++
@@ -1,5 +0,0 @@
-import { PipelineBuilder } from "@elaraai/core"
-import my_datastreams_template from "../gen/my_datastreams.template"
-
-
-export default PipelineBuilder(my_datastreams_template.streans["My Datastream"])
---
+++ 00_platform_essentials/03_transform_data/03_define_a_pipeline_operation/00/src/my_pipelines.ts
@@ -0,0 +1,5 @@
+import { PipelineBuilder } from "@elaraai/core"
+import my_datastreams from "../gen/my_datastreams.template"
+
+export default new PipelineBuilder(my_datastreams.tables["My Datastream"])
+    .toSchema("My Pipeline")
