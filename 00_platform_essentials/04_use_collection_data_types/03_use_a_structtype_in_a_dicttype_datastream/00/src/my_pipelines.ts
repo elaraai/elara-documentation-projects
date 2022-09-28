a1c2ff5554c6ccc7eeed34704b0e9199cf03dd7c
Ravind Raniga <ravindraniga@elara.ai>
2022-09-28 17:49:34 +1000

    fix: update to structtype starting point

--- 00_platform_essentials/04_use_collection_data_types/03_use_a_structtype_in_a_dicttype_datastream/00/src/my_pipelines.ts
+++ 00_platform_essentials/04_use_collection_data_types/03_use_a_structtype_in_a_dicttype_datastream/00/src/my_pipelines.ts
@@ -1,6 +1,6 @@
 import { Add, Default, IfNull, IntegerType, PipelineBuilder } from "@elaraai/core"
 import my_datastreams from "../gen/my_datastreams.template"
 
-export default new PipelineBuilder(my_datastreams.tables["My Datastream"])
+export default new PipelineBuilder(my_datastreams.tables["My IntegerType Datastream"])
     .transform(stream => IfNull(stream, Default(IntegerType), Add(stream, 1n)))
     .toSchema("My Pipeline")
