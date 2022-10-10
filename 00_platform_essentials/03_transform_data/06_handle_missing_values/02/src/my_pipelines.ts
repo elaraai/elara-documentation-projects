5f9cc293121975365887c1043369309e77ba5b2f
Ravind Raniga <ravindraniga@elara.ai>
2022-10-10 16:31:12 +1000

    fix: add intermediary stackblitz project for handle missing values

---
+++ 00_platform_essentials/03_transform_data/06_handle_missing_values/02/src/my_pipelines.ts
@@ -0,0 +1,6 @@
+import { Add, PipelineBuilder } from "@elaraai/core"
+import my_datastreams from "../gen/my_datastreams.template"
+
+export default new PipelineBuilder(my_datastreams.tables["My Datastream"])
+    .transform(stream => Add(stream, 1n))
+    .toTemplate("My Pipeline")
