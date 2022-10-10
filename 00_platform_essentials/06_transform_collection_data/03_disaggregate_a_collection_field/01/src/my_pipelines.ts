8c79a157bacfdfad830ae0bbd7bd0c7394a0c891
Ravind Raniga <ravindraniga@elara.ai>
2022-10-10 11:50:04 +1000

    middle project for 03_disaggregate_a_collection

---
+++ 00_platform_essentials/06_transform_collection_data/03_disaggregate_a_collection_field/01/src/my_pipelines.ts
@@ -0,0 +1,51 @@
+// import { Add, Default, GreaterEqual, IfNull, IntegerType, PipelineBuilder, Template } from "@elaraai/core"
+import { Add, Divide, Equal, Floor, GetField, Greater, GreaterEqual, PipelineBuilder, Range, Reduce, Template } from "@elaraai/core"
+// import my_datastreams from "../gen/my_datastreams.template"
+import my_datasources from "../gen/my_datasources.template"
+
+const sales = my_datasources.tables["Source.Sales"]
+
+// const transform_exercise =  new PipelineBuilder(my_datastreams.tables["My IntegerType Datastream"])
+//     .transform(stream => IfNull(stream, Default(IntegerType), Add(stream, 1n)))
+//     .toTemplate("My Transform")
+
+const filter_exercise_one = new PipelineBuilder(sales)
+    .filter(entry => GreaterEqual(entry.transactionDate, new Date(`2022-11-10`) ) )
+    .toTemplate("Filter After Datetime")
+
+const filter_exercise_two = new PipelineBuilder(sales)
+    .filter(entry => Equal(Floor(entry.transactionDate, "day"), new Date(`2022-11-10`) ) )
+    .toTemplate("Filter On Date")
+    
+const filter_exercise_three = new PipelineBuilder(sales)
+    .filter(
+        entry => Greater(
+            Reduce(
+                entry.items,
+                (previous, current) => Add(previous, GetField(current, "salePrice")),
+                0
+            ),
+            100.0
+        )
+    )
+    .toTemplate("Filter Revenue Greater than 100")
+
+const disaggregate_exercise_one = new PipelineBuilder(sales)
+    .disaggregateArray({
+        collection: (entry) => entry.items,
+        selections: {
+            transactionDate: (entry) => entry.transactionDate,
+            productCode: (_, item) => GetField(item, "productCode"),
+            units: (_, item) => GetField(item, "units"),
+            salePrice: (_, item) => GetField(item, "salePrice"),
+        },
+    })
+    .toTemplate("Disaggregate Units")
+
+export default Template(
+    // transform_exercise,
+    filter_exercise_one,
+    filter_exercise_two,
+    filter_exercise_three,
+    disaggregate_exercise_one
+)
