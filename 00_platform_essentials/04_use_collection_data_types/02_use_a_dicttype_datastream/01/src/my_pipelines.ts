3fe9e7a39e10eb2903dc7269b4d404c9ebd4b919
Ravind Raniga <ravindraniga@elara.ai>
2022-10-10 16:52:01 +1000

    fix: extra intermediary projects for using a dicttype datastream

---
+++ 00_platform_essentials/04_use_collection_data_types/02_use_a_dicttype_datastream/01/src/my_pipelines.ts
@@ -0,0 +1,6 @@
+import { Add, Default, IfNull, IntegerType, PipelineBuilder } from "@elaraai/core"
+import my_datastreams from "../gen/my_datastreams.template"
+
+export default new PipelineBuilder(my_datastreams.tables["My Datastream"])
+    .transform(stream => IfNull(stream, Default(IntegerType), Add(stream, 1n)))
+    .toTemplate("My Pipeline")
---
+++ 00_platform_essentials/04_use_collection_data_types/02_use_a_dicttype_datastream/02/src/my_pipelines.ts
@@ -0,0 +1,7 @@
+import { PipelineBuilder } from "@elaraai/core"
+import my_datastreams from "../gen/my_datastreams.template"
+
+export default new PipelineBuilder(my_datastreams.tables["My IntegerType Datastream"])
+    .transform(stream => IfNull(stream, Default(IntegerType), Add(stream, 1n)))
+    .toTemplate("My Pipeline")
+)
