e8190d8636adff8522d02853aa61dd465db57e91
Ravind Raniga <ravindraniga@elara.ai>
2022-10-11 12:35:39 +1000

    feat: intermediary exercise filter.exercise_two

---
+++ 00_platform_essentials/06_transform_collection_data/02_filter_a_collection/02/src/my_pipelines.ts
@@ -0,0 +1,24 @@
+// import { Add, Default, GreaterEqual, IfNull, IntegerType, PipelineBuilder, Template } from "@elaraai/core"
+import { Equal, Floor, GreaterEqual, PipelineBuilder, Template } from "@elaraai/core"
+// import my_datastreams from "../gen/my_datastreams.template"
+import my_datasources from "../gen/my_datasources.template"
+
+const sales = my_datasources.tables["Source.Sales"]
+
+// const transform_exercise =  new PipelineBuilder(my_datastreams.tables["My IntegerType Datastream"])
+//     .transform(stream => IfNull(stream, Default(IntegerType), Add(stream, 1n)))
+//     .toTemplate("My Transform")
+
+const filter_exercise_one = new PipelineBuilder(sales)
+    .filter(entry => GreaterEqual(entry.transactionDate, new Date(`2022-11-10`) ) )
+    .toTemplate("Filter After Datetime")
+
+const filter_exercise_two = new PipelineBuilder(sales)
+    .filter(entry => Equal(Floor(entry.transactionDate, "day"), new Date(`2022-11-10`) ) )
+    .toTemplate("Filter On Date")
+
+export default Template(
+    // transform_exercise,
+    filter_exercise_one,
+    filter_exercise_two
+)
