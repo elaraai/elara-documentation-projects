68c07243dd64b6fbc7e2908d412686a3984debbf
Ravind Raniga <ravindraniga@elara.ai>
2022-11-16 16:54:15 +1000

    fix: update to single def file

--- 00_platform_essentials/03_transform_data/06_handle_missing_values/01/src/my_tutorial.ts
+++ 00_platform_essentials/03_transform_data/06_handle_missing_values/01/src/my_tutorial.ts
@@ -1,6 +1,20 @@
-import { IntegerType, Nullable, SourceBuilder } from "@elaraai/core"
+import { Equal, IfElse, IntegerType, Nullable, PipelineBuilder, SourceBuilder, Template } from "@elaraai/core"
 
 
-export default new SourceBuilder("My Datastream")
+const my_datastream = new SourceBuilder("My Datastream")
     .writeable(Nullable(IntegerType))
-    .toTemplate()
+
+const my_pipeline = new PipelineBuilder("My Pipeline")
+    .from(my_datastream.outputStream())
+    .transform(
+        stream => IfElse(
+            Equal(stream, 10n),
+            100n,
+            1n
+        )
+    )
+
+export default Template(
+    my_datastream.toTemplate(),
+    my_pipeline.toTemplate()
+)
