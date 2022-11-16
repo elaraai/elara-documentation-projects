3540a9101b78989426410cfef2289445db23b9fb
Ravind Raniga <ravindraniga@elara.ai>
2022-11-16 17:02:48 +1000

    fix: remove file

--- 00_platform_essentials/03_transform_data/06_handle_missing_values/02/src/my_pipelines.ts
+++
@@ -1,6 +0,0 @@
-import { Add, PipelineBuilder } from "@elaraai/core"
-import my_tutorial from "../gen/my_tutorial.template"
-
-export default new PipelineBuilder(my_tutorial.tables["My Datastream"])
-    .transform(stream => Add(stream, 1n))
-    .toTemplate("My Pipeline")
