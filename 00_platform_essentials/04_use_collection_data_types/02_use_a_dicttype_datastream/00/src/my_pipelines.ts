d969ddb67914ba13b4c910fbe64a51580f890f1c
Ravind Raniga <ravindraniga@elara.ai>
2022-11-16 17:13:09 +1000

    fix: reduce to single def file

--- 00_platform_essentials/04_use_collection_data_types/02_use_a_dicttype_datastream/00/src/my_pipelines.ts
+++
@@ -1,6 +0,0 @@
-import { Add, Default, IfNull, IntegerType, PipelineBuilder } from "@elaraai/core"
-import my_tutorial from "../gen/my_tutorial.template"
-
-export default new PipelineBuilder(my_tutorial.tables["My Datastream"])
-    .transform(stream => IfNull(stream, Default(IntegerType), Add(stream, 1n)))
-    .toTemplate("My Pipeline")
