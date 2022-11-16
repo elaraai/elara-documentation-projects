e8ec1da25b62da76ebbf10950fc622773a626ec0
Ravind Raniga <ravindraniga@elara.ai>
2022-11-16 15:48:28 +1000

    fix: add gen files to show in output of build output

---
+++ 00_platform_essentials/02_get_data_in_and_out/03_launch_a_solution/01/gen/my_tutorial.template.ts
@@ -0,0 +1,13 @@
+// GENERATED FILE, DO NOT EDIT
+export default {
+datasources: {},
+pipelines: {},
+pipelines2: {},
+models: {},
+environments: {},
+streams: {},
+tasks: {},
+defaults: {},
+scenarios: {},
+}
+// GENERATED FILE, DO NOT EDIT
