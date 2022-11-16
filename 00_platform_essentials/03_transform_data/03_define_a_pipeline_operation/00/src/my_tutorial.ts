6338c1c21921e9bfec29422b482863e55a603608
Ravind Raniga <ravindraniga@elara.ai>
2022-11-16 11:41:25 +1000

    fix: define pipeline in one file

--- 00_platform_essentials/03_transform_data/03_define_a_pipeline_operation/00/src/my_tutorial.ts
+++ 00_platform_essentials/03_transform_data/03_define_a_pipeline_operation/00/src/my_tutorial.ts
@@ -1,6 +1,13 @@
-import { IntegerType, SourceBuilder } from "@elaraai/core"
+import { IntegerType, PipelineBuilder, SourceBuilder, Template } from "@elaraai/core"
 
 
-export default new SourceBuilder("My Datastream")
+const my_datastream = new SourceBuilder("My Datastream")
     .writeable(IntegerType)
-    .toTemplate()
+
+const my_pipeline = new PipelineBuilder("My Pipeline")
+    .from(my_datastream.outputStream())
+
+export default Template(
+    my_datastream.toTemplate(),
+    my_pipeline.toTemplate()
+)
