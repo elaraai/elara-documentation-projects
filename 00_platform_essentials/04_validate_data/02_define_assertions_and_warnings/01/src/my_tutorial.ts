e8413eaf3b4cd68df1c0bc6af81f08e927b202d1
Ravind Raniga <ravindraniga@elara.ai>
2022-11-17 16:02:58 +1000

    fix: add warning and assertion operations to test project

--- 00_platform_essentials/04_validate_data/02_define_assertions_and_warnings/01/src/my_tutorial.ts
+++ 00_platform_essentials/04_validate_data/02_define_assertions_and_warnings/01/src/my_tutorial.ts
@@ -1,4 +1,4 @@
-import { Default, IfNull, IntegerType, Nullable, PipelineBuilder, SourceBuilder, Template } from "@elaraai/core"
+import { Add, Default, IfNull, IntegerType, Less, Nullable, PipelineBuilder, SourceBuilder, StringJoin, Template } from "@elaraai/core"
 
 
 const my_datastream = new SourceBuilder("My Datastream")
@@ -7,6 +7,15 @@ const my_datastream = new SourceBuilder("My Datastream")
 const my_pipeline = new PipelineBuilder("My Pipeline")
     .from(my_datastream.outputStream())
     .transform(stream => IfNull(stream, Default(IntegerType)))
+    .warn({
+        predicate: stream => Less(stream, 40n),
+        message: stream => StringJoin`Expected value less than 40, got ${stream}`
+    })
+    .assert({
+        predicate: stream => Less(stream, 50n),
+        message: stream => StringJoin`Expected value less than 50, got ${stream}`
+    })
+    .transform(stream => Add(stream, 1n))
 
 export default Template(
     my_datastream.toTemplate(),
