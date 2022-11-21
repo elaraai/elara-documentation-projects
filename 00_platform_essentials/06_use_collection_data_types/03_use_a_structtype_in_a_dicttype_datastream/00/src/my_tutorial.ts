bbe2f6bd4a45be389ca22482c270a3034f4c52ec
Ravind Raniga <ravindraniga@elara.ai>
2022-11-21 15:40:19 +1000

    refactor: starting point for structtype page

--- 00_platform_essentials/06_use_collection_data_types/03_use_a_structtype_in_a_dicttype_datastream/00/src/my_tutorial.ts
+++ 00_platform_essentials/06_use_collection_data_types/03_use_a_structtype_in_a_dicttype_datastream/00/src/my_tutorial.ts
@@ -1,15 +1,27 @@
-import { DictType, IntegerType, Nullable, SourceBuilder, StringType, Template } from "@elaraai/core"
+import { Add, Default, DictType, IfNull, IntegerType, Less, Nullable, PipelineBuilder, SourceBuilder, StringJoin, StringType, Template } from "@elaraai/core"
 
 
-const my_integertype_datastream = new SourceBuilder("My IntegerType Datastream")
+const my_datastream = new SourceBuilder("My Datastream")
     .writeable(Nullable(IntegerType))
-    .toTemplate()
+
+const my_pipeline = new PipelineBuilder("My Pipeline")
+    .from(my_datastream.outputStream())
+    .transform(stream => IfNull(stream, Default(IntegerType)))
+    .assert({
+        predicate: stream => Less(stream, 50n),
+        message: stream => StringJoin`Expected value less than 50, got ${stream}`
+    })
+    .warn({
+        predicate: stream => Less(stream, 40n),
+        message: stream => StringJoin`Expected value less than 40, got ${stream}`
+    })
+    .transform(stream => Add(stream, 1n))
 
 const my_dicttype_datastream = new SourceBuilder("My DictType Datastream")
     .writeable(DictType(StringType, IntegerType))
-    .toTemplate()
 
 export default Template(
-    my_integertype_datastream,
-    my_dicttype_datastream
+    my_datastream.toTemplate(),
+    my_dicttype_datastream.toTemplate(),
+    my_pipeline.toTemplate()
 )
