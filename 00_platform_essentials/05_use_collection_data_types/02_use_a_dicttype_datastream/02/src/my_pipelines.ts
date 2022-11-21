f981a5ad7b116a1a59e3e318c6ded44679ab5e26
Ravind Raniga <ravindraniga@elara.ai>
2022-11-21 15:25:36 +1000

    refactor: convert use-collection projects to new format

--- 00_platform_essentials/05_use_collection_data_types/02_use_a_dicttype_datastream/02/src/my_pipelines.ts
+++
@@ -1,7 +0,0 @@
-import { PipelineBuilder } from "@elaraai/core"
-import my_tutorial from "../gen/my_tutorial.template"
-
-export default new PipelineBuilder(my_tutorial.tables["My IntegerType Datastream"])
-    .transform(stream => IfNull(stream, Default(IntegerType), Add(stream, 1n)))
-    .toTemplate("My Pipeline")
-)
---
+++ 00_platform_essentials/06_use_collection_data_types/02_use_a_dicttype_datastream/01/src/my_tutorial.ts
@@ -0,0 +1,27 @@
+import { Add, Default, DictType, IfNull, IntegerType, Less, Nullable, PipelineBuilder, SourceBuilder, StringJoin, StringType, Template } from "@elaraai/core"
+
+
+const my_datastream = new SourceBuilder("My Datastream")
+    .writeable(Nullable(IntegerType))
+
+const my_pipeline = new PipelineBuilder("My Pipeline")
+    .from(my_datastream.outputStream())
+    .transform(stream => IfNull(stream, Default(IntegerType)))
+    .assert({
+        predicate: stream => Less(stream, 50n),
+        message: stream => StringJoin`Expected value less than 50, got ${stream}`
+    })
+    .warn({
+        predicate: stream => Less(stream, 40n),
+        message: stream => StringJoin`Expected value less than 40, got ${stream}`
+    })
+    .transform(stream => Add(stream, 1n))
+
+const my_dicttype_datastream = new SourceBuilder("My DictType Datastream")
+    .writeable(DictType(StringType, IntegerType))
+
+export default Template(
+    my_datastream.toTemplate(),
+    my_dicttype_datastream.toTemplate(),
+    my_pipeline.toTemplate()
+)
