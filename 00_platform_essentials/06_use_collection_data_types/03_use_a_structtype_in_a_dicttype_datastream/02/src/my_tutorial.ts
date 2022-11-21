08856e23c5e1089c5a33b3f871430dc9b3698723
Ravind Raniga <ravindraniga@elara.ai>
2022-11-21 16:02:06 +1000

    fix: more fixes to collections

---
+++ 00_platform_essentials/06_use_collection_data_types/03_use_a_structtype_in_a_dicttype_datastream/02/src/my_tutorial.ts
@@ -0,0 +1,35 @@
+import { Add, Default, DictType, IfNull, IntegerType, Less, Nullable, PipelineBuilder2, SourceBuilder, StringJoin, StringType, StructType, Template } from "@elaraai/core"
+
+
+const my_datastream = new SourceBuilder("My Datastream")
+    .writeable(Nullable(IntegerType))
+
+const my_pipeline = new PipelineBuilder2("My Pipeline")
+    .from(my_datastream.outputStream())
+    .transform(stream => IfNull(stream, Default(IntegerType)))
+    .assert({
+        predicate: stream => Less(stream, 50n),
+        message: stream => StringJoin`Expected value less than 50, got ${stream}`
+    })
+    .warn({
+        predicate: stream => Less(stream, 40n),
+        message: stream => StringJoin`Expected value less than 40, got ${stream}`
+    })
+    .transform(stream => Add(stream, 1n))
+
+const my_dicttype_datastream = new SourceBuilder("My DictType Datastream")
+    .writeable(
+        DictType(
+            StringType,
+            StructType({
+                my_integer: IntegerType,
+                my_string: StringType
+            })
+        )
+    )
+
+export default Template(
+    my_datastream.toTemplate(),
+    my_dicttype_datastream.toTemplate(),
+    my_pipeline.toTemplate()
+)
