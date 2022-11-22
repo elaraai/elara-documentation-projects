02f17fb2461ca97e125b0b67c21768d5c76b7269
Ravind Raniga <ravindraniga@elara.ai>
2022-11-22 15:15:46 +1000

    feat: work with blobtype data project

---
+++ 00_platform_essentials/07_use_blobtype_data/02_work_with_a_blobtype_datastream/00/src/my_tutorial.ts
@@ -0,0 +1,39 @@
+import { Add, BlobType, Default, DictType, IfNull, IntegerType, Less, Nullable, PipelineBuilder, SourceBuilder, StringJoin, StringType, StructType, Template } from "@elaraai/core"
+
+
+const my_datastream = new SourceBuilder("My Datastream")
+    .writeable(Nullable(IntegerType))
+
+const my_pipeline = new PipelineBuilder("My Pipeline")
+    .from(my_datastream.outputStream())
+    .transform(stream => IfNull(stream, Default(IntegerType)))
+    .assert({
+        predicate: stream => Less(stream, 50n),
+        message: stream => StringJoin`Expected value less than 50, got ${stream}`
+    })
+    .warn({
+        predicate: stream => Less(stream, 40n),
+        message: stream => StringJoin`Expected value less than 40, got ${stream}`
+    })
+    .transform(stream => Add(stream, 1n))
+
+const my_dicttype_datastream = new SourceBuilder("My DictType Datastream")
+    .writeable(
+        DictType(
+            StringType,
+            StructType({
+                my_integer: IntegerType,
+                my_string: StringType
+            })
+        )
+    )
+
+const my_blobtype_datastream = new SourceBuilder("My BlobType Datastream")
+    .writeable(BlobType)
+
+export default Template(
+    my_datastream,
+    my_dicttype_datastream,
+    my_pipeline,
+    my_blobtype_datastream
+)
