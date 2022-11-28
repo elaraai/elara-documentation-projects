bb794276bcb02d7d5a717871e0b8ac0d47729a5d
Ravind Raniga <ravindraniga@elara.ai>
2022-11-28 16:22:34 +1000

    fix: remove elara.zip

--- 00_platform_essentials/10_transform_collection_data/03_filter_a_collection/00/src/my_tutorial.ts
+++ 00_platform_essentials/10_transform_collection_data/03_filter_a_collection/00/src/my_tutorial.ts
@@ -1,9 +1,21 @@
-import { DictType, IntegerType, Nullable, SourceBuilder, StringType, StructType, Template } from "@elaraai/core"
+import { Add, BlobType, Default, DictType, FloatType, Greater, IfNull, IntegerType, Less, Nullable, PipelineBuilder, Size, SourceBuilder, StringJoin, StringType, StructType, Template } from "@elaraai/core"
 
 
-const my_integertype_datastream = new SourceBuilder("My IntegerType Datastream")
+const my_datastream = new SourceBuilder("My Datastream")
     .writeable(Nullable(IntegerType))
-    .toTemplate()
+
+const my_pipeline = new PipelineBuilder("My Pipeline")
+    .from(my_datastream.outputStream())
+    .transform(stream => IfNull(stream, Default(IntegerType)))
+    .assert({
+        predicate: stream => Less(stream, 50n),
+        message: stream => StringJoin`Expected value less than 50, got ${stream}`
+    })
+    .warn({
+        predicate: stream => Less(stream, 40n),
+        message: stream => StringJoin`Expected value less than 40, got ${stream}`
+    })
+    .transform(stream => Add(stream, 1n))
 
 const my_dicttype_datastream = new SourceBuilder("My DictType Datastream")
     .writeable(
@@ -15,9 +27,36 @@ const my_dicttype_datastream = new SourceBuilder("My DictType Datastream")
             })
         )
     )
-    .toTemplate()
+
+const my_blobtype_datastream = new SourceBuilder("My BlobType Datastream")
+    .writeable(BlobType)
+    .assert({
+        predicate: stream => Greater(Size(stream), 3n),
+        message: stream => StringJoin`Expected file with 3 or more bytes, got ${Size(stream)} bytes.`
+    })
+
+const my_products_file_source = new SourceBuilder("Products")
+    .file({ path: "./data/products.csv" })
+
+const parse_products = new PipelineBuilder("Parse Products")
+    .from(my_products_file_source.outputStream())
+    .fromCsv({
+        skip_n: 1n,
+        delimiter: ",",
+        fields: {
+            Name: StringType,
+            Code: StringType,
+            Category: StringType,
+            "Unit Cost": Nullable(FloatType)
+        },
+        output_key: fields => fields.Code
+    })
 
 export default Template(
-    my_integertype_datastream,
-    my_dicttype_datastream
+    my_datastream,
+    my_dicttype_datastream,
+    my_pipeline,
+    my_blobtype_datastream,
+    my_products_file_source,
+    parse_products
 )
