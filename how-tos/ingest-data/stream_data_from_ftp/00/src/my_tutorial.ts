0130c819d3449ac8adee46ddf74a56a6e0597daa
Ravind Raniga <ravindraniga@elara.ai>
2022-12-09 15:49:54 +1000

    fix: add caveats

--- how-tos/ingest-data/stream_data_from_ftp/00/src/my_tutorial.ts
+++ how-tos/ingest-data/stream_data_from_ftp/00/src/my_tutorial.ts
@@ -1,5 +1,6 @@
 import { Const, SourceBuilder, Template } from "@elaraai/core"
 
+// Note that FTP server accessible below is courtesy of <Link href="https://www.wftpserver.com/onlinedemo.htm">Wing FTP Server</Link>. It is not maintained or affiliated with ELARA. Use this service at your own risk.
 const protocol = "ftp://"
 const url = "demo.wftpserver.com"
 const file_path = "/download/version.txt"
