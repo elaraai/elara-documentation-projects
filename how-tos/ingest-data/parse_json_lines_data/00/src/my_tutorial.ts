e8fe4260c0242369ca2389a93de801d64028120d
Ravind Raniga <ravindraniga@elara.ai>
2022-12-16 15:37:46 +1000

    feat: add example project for parse json lines

---
+++ how-tos/ingest-data/parse_json_lines_data/00/src/my_tutorial.ts
@@ -0,0 +1,30 @@
+import { BlobType, SourceBuilder, PipelineBuilder, StringType, Template, ArrayType, StructType, FloatType } from "@elaraai/core"
+
+const my_blobtype_stream = new SourceBuilder("My BlobType Stream")
+    .value({
+        value: new Uint8Array(
+            new TextEncoder().encode(
+                '{"name": "Gilbert", "sales": [{"salePrice": "99.90"}, {"salePrice": "35.90"}]}\n{"name": "Alexa", "sales": [{"salePrice": "23.90"}, {"salePrice": "45.90"}]}\n{"name": "May", "sales": []}\n{"name": "Deloise", "sales": [{"salePrice": "99.90"}}'
+            )
+        ),
+        type: BlobType
+    })
+
+const my_parser = new PipelineBuilder("My Parser")
+    .from(my_blobtype_stream.outputStream())
+    .fromJsonLines({
+        fields: {
+            name: StringType,
+            sales: ArrayType(
+                StructType({
+                    salePrice: FloatType
+                })
+            )
+        },
+        output_key: fields => fields.name
+    })
+
+export default Template(
+    my_blobtype_stream,
+    my_parser
+)
