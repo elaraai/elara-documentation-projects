bed11fda20452c200c96931ef74fb16d613e6dd4
Ravind Raniga <ravindraniga@elara.ai>
2023-02-16 13:55:48 +1000

    feat: train model - TODO: test

--- modelling_essentials/03_create_predictive_insights/03_train_an_ml_model/00/src/model.ts
+++ modelling_essentials/03_create_predictive_insights/03_train_an_ml_model/00/src/model.ts
@@ -1,82 +1,88 @@
-import { AddDuration, Convert, Duration, FloatType, IfElse, IfNull, MLModelBuilder, PipelineBuilder, Round, SourceBuilder, Template } from "@elaraai/core"
+import { Convert, DateTimeType, FloatType, IntegerType, MLModelBuilder, PipelineBuilder, Print, SourceBuilder, StringType, Template } from "@elaraai/core"
 
-const sales_data = new SourceBuilder("Sales Records")
-    .value({
-        value: new Map([
-            ["0", { date: new Date(`2022-10-10T09:00:00.000Z`), qty: 1n, price: 4.00 }],
-            ["1", { date: new Date(`2022-10-10T10:00:00.000Z`), qty: 2n, price: 4.00 }],
-            ["2", { date: new Date(`2022-10-10T11:00:00.000Z`), qty: 3n, price: 4.00 }],
-            ["3", { date: new Date(`2022-10-10T12:00:00.000Z`), qty: 2n, price: 4.00 }],
-            ["4", { date: new Date(`2022-10-10T13:00:00.000Z`), qty: 2n, price: 4.00 }],
-            ["5", { date: new Date(`2022-10-11T09:00:00.000Z`), qty: 1n, price: 4.50 }],
-            ["6", { date: new Date(`2022-10-11T10:30:00.000Z`), qty: 1n, price: 4.50 }],
-            ["7", { date: new Date(`2022-10-11T12:00:00.000Z`), qty: 1n, price: 4.50 }],
-            ["8", { date: new Date(`2022-10-11T13:30:00.000Z`), qty: 2n, price: 4.50 }],
-            ["9", { date: new Date(`2022-10-12T09:00:00.000Z`), qty: 5n, price: 3.50 }],
-            ["10", { date: new Date(`2022-10-12T09:30:00.000Z`), qty: 4n, price: 3.50 }],
-            ["11", { date: new Date(`2022-10-12T10:00:00.000Z`), qty: 3n, price: 3.50 }],
-            ["12", { date: new Date(`2022-10-12T11:00:00.000Z`), qty: 3n, price: 3.50 }],
-            ["13", { date: new Date(`2022-10-12T11:30:00.000Z`), qty: 2n, price: 3.50 }],
-            ["14", { date: new Date(`2022-10-12T12:00:00.000Z`), qty: 4n, price: 3.50 }],
-            ["15", { date: new Date(`2022-10-12T13:00:00.000Z`), qty: 4n, price: 3.50 }],
-            ["16", { date: new Date(`2022-10-12T13:30:00.000Z`), qty: 3n, price: 3.50 }],
-        ])
-    })
+const sales_file = new SourceBuilder("Sales File")
+    .file({ path: 'data/sales.jsonl' })
 
-const qty_training_data = new PipelineBuilder("Transform Quantity for ML")
-    .from(sales_data.outputStream())
-    .select({
-        keep_all: false,
-        selections: {
-            price: fields => fields.price,
-            qty: fields => Convert(fields.qty, FloatType)
-        }
-    })
+const suppliers_file = new SourceBuilder("Suppliers File")
+    .file({ path: 'data/suppliers.jsonl' })
 
-const qty_ml_model = new MLModelBuilder("Quantity Model")
-    .feature("price", FloatType)
-    .output(FloatType)
-    .model({ type: "linear", noise: "none" })
-      .train({
-        output_name: "qty",
-        input: qty_training_data.outputStream(),
-      })
+const procurement_file = new SourceBuilder("Procurement File")
+    .file({ path: 'data/procurement.jsonl' })
 
-const arrival_time_training_data = new PipelineBuilder("Transform Arrival Time for ML")
-    .from(sales_data.outputStream())
-    .offset({
-        sort_key: fields => fields.date,
-        group_key: fields => Round(fields.date, "floor", "day"),
-        offset: -1,
-        offset_selections: {
-            previousDate: (fields, _, exists) => IfElse(
-                exists,
-                fields.date,
-                AddDuration(Round(fields.date, "floor", "day"), 9, "hour")
-            ),
+// parse the blob data into jsonl data
+const procurement_data = new PipelineBuilder('Historic Procurement')
+    .from(procurement_file.outputStream())
+    .fromJsonLines({
+        fields: {
+            // the date of a historic procurement - these occr daily
+            date: DateTimeType,
+            // the supplier the procurement was from
+            supplierName: StringType,
         },
-    })
-    .select({
-        keep_all: false,
-        selections: {
-            price: fields => fields.price,
-            interArrivalHours: fields => IfNull(Duration(fields.previousDate, fields.date, "hour"), 0)
-        }
+        // the procurement date is unique, so can be used as the key
+        output_key: fields => Print(fields.date)
+    });
+
+const sales_data = new PipelineBuilder('Historic Sales')
+    .from(sales_file.outputStream())
+    .fromJsonLines({
+        fields: {
+            // the date of our aggregate sales records
+            date: DateTimeType,
+            // the qty of suasages sold in the hour
+            qty: IntegerType,
+            // the discount applied duration that hour
+            discount: FloatType,
+        },
+        // the sale date is unique, so can be used as the key
+        output_key: fields => Print(fields.date)
     })
 
-const arrival_time_ml_model = new MLModelBuilder("Arrival Time Model")
-    .feature("price", FloatType)
+const supplier_data = new PipelineBuilder('Suppliers')
+    .from(suppliers_file.outputStream())
+    .fromJsonLines({
+        fields: {
+            // the name of the supplier
+            supplierName: StringType,
+            // the number of days until payment from order
+            paymentTerms: FloatType,
+            // the number of days it takes to recieve suasages
+            leadTime: FloatType,
+            // the cost per sausage
+            unitCost: FloatType,
+            // the amount of sausages that must be ordered
+            orderQty: IntegerType,
+        },
+        // the name is unique, so can bs used as the key
+        output_key: fields => fields.supplierName
+    })
+
+// ML Model
+const demand = new MLModelBuilder("Demand")
+    // add a feature to the model
+    .feature("discount", FloatType)
+    // the output is qty which is FloatType
     .output(FloatType)
-    .model({ type: "linear", noise: "none" })
-      .train({
-        output_name: "interArrivalHours",
-        input: arrival_time_training_data.outputStream(),
-      })
+    // define the historic data as the training set
+    .trainFromPipeline({
+        output_name: "qty",
+        pipeline: builder => builder
+            .from(sales_data.outputStream())
+            // select just the discount and qty from the sale for training
+            .select({
+                selections: {
+                    discount: fields => fields.discount,
+                    qty: fields => Convert(fields.qty, FloatType)
+                }
+            })
+    });
 
 export default Template(
+    sales_file,
+    suppliers_file,
+    procurement_file,
+    procurement_data,
     sales_data,
-    qty_training_data,
-    qty_ml_model,
-    arrival_time_training_data,
-    arrival_time_ml_model,
-);
+    supplier_data,
+    demand
+)
