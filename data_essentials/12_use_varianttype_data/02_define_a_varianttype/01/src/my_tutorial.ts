32c072294546277adee65a6907634be6bb4a60da
Ravind Raniga <ravindraniga@elara.ai>
2023-03-07 11:29:44 +1000

    fix: revert to buggy version

--- data_essentials/12_use_varianttype_data/02_define_a_varianttype/01/src/my_tutorial.ts
+++ data_essentials/12_use_varianttype_data/02_define_a_varianttype/01/src/my_tutorial.ts
@@ -1,17 +1,14 @@
-import { BooleanType, SourceBuilder, StringType, Template, VariantType } from "@elaraai/core"
+import { BooleanType, SourceBuilder, StringType, Template, Variant, VariantType } from "@elaraai/core"
 
 const varianttype_datastream = new SourceBuilder("My VariantType Datastream")
-    // .value({
-    //     value: Variant("a", true),
-    //     type: VariantType({
-    //         a: BooleanType,
-    //         b: StringType,
-    //     })
-    // })
-    .writeable(VariantType({
-        a: BooleanType,
-        b: StringType,
-    }))
+    .value({
+        value: Variant("a", true),
+        type: VariantType({
+            a: BooleanType,
+            b: StringType,
+        })
+    })
+
 
 export default Template(
     varianttype_datastream
