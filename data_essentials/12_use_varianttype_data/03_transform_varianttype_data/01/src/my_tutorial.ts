5a60e5006c673887d10b8e1e2fe31feeb9d3097a
Ravind Raniga <ravindraniga@elara.ai>
2023-03-07 16:14:44 +1000

    feat: transform second-project

--- data_essentials/12_use_varianttype_data/03_transform_varianttype_data/01/src/my_tutorial.ts
+++ data_essentials/12_use_varianttype_data/03_transform_varianttype_data/01/src/my_tutorial.ts
@@ -1,4 +1,4 @@
-import { BooleanType, IfElse, NewVariant, PipelineBuilder, SourceBuilder, StringType, Template, Variant, VariantType } from "@elaraai/core"
+import { BooleanType, IfElse, Match, NewVariant, PipelineBuilder, SourceBuilder, StringJoin, StringType, Template, Variant, VariantType } from "@elaraai/core"
 
 const varianttype_datastream = new SourceBuilder("My VariantType Datastream")
     .value({
@@ -20,8 +20,21 @@ const my_pipeline = new PipelineBuilder("Construct a Variant")
         NewVariant("b", "some string")
     ))
 
+const my_deconstruct_pipeline = new PipelineBuilder("Deconstruct a Variant")
+    .from(varianttype_datastream.outputStream())
+    .transform(
+        stream => Match(
+            stream,
+            {
+                a: data => StringJoin`My stream has data: ${data}`,
+                b: data => StringJoin`My stream has data: ${data}`
+            },
+        )
+    )
+
 export default Template(
     varianttype_datastream,
     my_boolean_datastream,
-    my_pipeline
+    my_pipeline,
+    my_deconstruct_pipeline
 )
