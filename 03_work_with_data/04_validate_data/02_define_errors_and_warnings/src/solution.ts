c4b6e1dc99276aa128053845c57b75f124a884d4
crambelsoupy <cmorrison@elara.ai>
2023-11-17 03:40:24 +0000

    doc: move collection types

--- 03_work_with_data/04_validate_data/02_define_errors_and_warnings/src/solution.ts
+++ 03_work_with_data/04_validate_data/02_define_errors_and_warnings/src/solution.ts
@@ -1,4 +1,4 @@
-import { SourceBuilder, PipelineBuilder, Template, Add, Const, Less, StringJoin } from "@elaraai/core"
+import { SourceBuilder, PipelineBuilder, Template, Add, Const, StringJoin, Greater } from "@elaraai/core"
 
 const my_source = new SourceBuilder("My Source")
     .value({ value: 2n })
@@ -7,8 +7,16 @@ const my_pipeline = new PipelineBuilder("My Pipeline")
     .from(my_source.outputStream())
     .transform((stream) =>  Add(stream, Const(1n)))
     .error({
-        if: stream => Less(stream, Const(50n)),
-        message: stream => StringJoin`Expected value less than 50, got ${stream}`
+        if: stream => Greater(stream, Const(50n)),
+        message: stream => StringJoin`Require value less than 50, got ${stream}`
+    })
+    .warn({
+        if: stream => Greater(stream, Const(20n)),
+        message: stream => StringJoin`Prefer value less than 20, got ${stream}`
+    })
+    .log({
+        if: stream => Greater(stream, Const(10n)),
+        message: stream => StringJoin`Noticed value less than 10, got ${stream}`
     })
     
 export default Template(my_source, my_pipeline)   
---
+++ 03_work_with_data/05_use_complex_data_types/02_use_a_collection_type/src/solution.ts
@@ -0,0 +1,9 @@
+import { SourceBuilder, Template } from "@elaraai/core"
+
+const my_source = new SourceBuilder("My Source")
+    .value({ value: new Map([
+        ["0", 1n],
+        ["1", 2n],
+    ]) })
+
+export default Template(my_source) 
