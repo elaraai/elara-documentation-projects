0b61e49465be2d5863603bc34b08d554c2bc7c72
crambelsoupy <cmorrison@elara.ai>
2023-11-17 04:43:17 +0000

    doc: 03_use_a_compound_type

--- 03_work_with_data/05_use_complex_data_types/03_use_a_compound_type/src/solution.ts
+++ 03_work_with_data/05_use_complex_data_types/03_use_a_compound_type/src/solution.ts
@@ -1,3 +1,9 @@
-import { Template } from "@elaraai/core"
+import { SourceBuilder, Template } from "@elaraai/core"
 
-export default Template();
+const my_source = new SourceBuilder("My Source")
+    .value({ value: new Map([
+        ["0", { value: 1n }],
+        ["1", { value: 2n }],
+    ]) })
+
+export default Template(my_source) 
