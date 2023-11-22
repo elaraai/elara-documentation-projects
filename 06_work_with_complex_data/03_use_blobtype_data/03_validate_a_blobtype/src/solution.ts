58dbf48ec4c254d7b9a59b06e79945c695d0ccd7
crambelsoupy <cmorrison@elara.ai>
2023-11-22 06:04:50 +0000

    doc: complex data

--- 06_work_with_complex_data/03_use_blobtype_data/03_validate_a_blobtype/src/solution.ts
+++ 06_work_with_complex_data/03_use_blobtype_data/03_validate_a_blobtype/src/solution.ts
@@ -1,3 +1,10 @@
-import { Template } from "@elaraai/core"
+import { BlobType, Less, Size, SourceBuilder, Template, Const, StringJoin } from "@elaraai/core"
 
-export default Template();
+const my_source = new SourceBuilder("My Source")
+    .writeable(BlobType)
+    .error({
+        if: value => Less(Size(value), Const(0n)),
+        message: () => StringJoin`File is empty`,
+    })
+
+export default Template(my_source)
--- 06_work_with_complex_data/03_use_blobtype_data/07_unparse_to_blob_data/src/solution.ts
+++ 06_work_with_complex_data/03_use_blobtype_data/07_unparse_to_blob_data/src/solution.ts
@@ -1,3 +1,29 @@
-import { Template } from "@elaraai/core"
+import { PipelineBuilder, SourceBuilder, Template, ToArray, ToCsv } from "@elaraai/core"
 
-export default Template();
+const my_source = new SourceBuilder("My Source")
+    .value({
+        value: new Map([
+            ["01", { string: "0mtsqttl4s5", date: new Date(), float: -790.29, integer: -875, boolean: false }],
+            ["02", { string: "0mtsqttl4s5", date: new Date(), float: -790.29, integer: -875, boolean: false }],
+        ])
+    });
+
+const my_pipeline = new PipelineBuilder("My Pipeline")
+    .from(my_source.outputStream())
+    .toCsv({
+        selections: {
+            string: (fields) => fields.string,
+            date: (fields) => fields.date,
+            float: (fields) => fields.float,
+            integer: (fields) => fields.integer,
+            boolean: (fields) => fields.boolean,
+        },
+    });
+
+const my_other_pipeline = new PipelineBuilder("My Other Pipeline")
+    .from(my_source.outputStream())
+    .transform(input => ToCsv(
+        ToArray(input),
+    ))
+
+export default Template(my_source, my_pipeline, my_other_pipeline)
