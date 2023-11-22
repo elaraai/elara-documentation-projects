c577f4779c9a84893a70925225a18933867f9166
crambelsoupy <cmorrison@elara.ai>
2023-11-22 20:45:31 +0000

    doc: 03_use_blobtype_dat

--- 06_work_with_complex_data/03_use_blobtype_data/03_validate_a_blobtype/src/solution.ts
+++ 06_work_with_complex_data/03_use_blobtype_data/03_validate_a_blobtype/src/solution.ts
@@ -1,10 +1,10 @@
-import { BlobType, Less, Size, SourceBuilder, Template, Const, StringJoin } from "@elaraai/core"
+import { BlobType, Size, SourceBuilder, Template, Const, Equal } from "@elaraai/core"
 
 const my_source = new SourceBuilder("My Source")
     .writeable(BlobType)
     .error({
-        if: value => Less(Size(value), Const(0n)),
-        message: () => StringJoin`File is empty`,
+        if: value => Equal(Size(value), Const(5n)),
+        message: () => Const(`File is empty`),
     })
 
 export default Template(my_source)
