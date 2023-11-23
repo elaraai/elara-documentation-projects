4c3a07f2e26919d5210c34fd85e8974b3681e3ee
Andy Ferris <ferris.andy@gmail.com>
2023-11-23 17:04:13 +1000

    Custom task and scenario projects

--- 08_work_with_custom_tasks/01_custom_tasks/02_define_a_custom_task/src/solution.ts
+++ 08_work_with_custom_tasks/01_custom_tasks/02_define_a_custom_task/src/solution.ts
@@ -1,3 +1,19 @@
-import { Template } from "@elaraai/core"
+import { CustomTaskBuilder, FloatType, Parse, Print, SourceBuilder, Template, Utf8Decode, Utf8Encode } from "@elaraai/core"
 
-export default Template();
+const my_source = new SourceBuilder("My Source")
+    .writeable(FloatType);
+
+const custom_task = new CustomTaskBuilder("Quadratic")
+    .input(
+        "input.txt", // filename
+        my_source.outputStream(), // datastream
+        false, // executable
+        float => Utf8Encode(Print(float)), // toBlob
+    )
+    .shell(`awk '{x=$1; printf "%.14f\\n", x * (10 - x)}' input.txt > output.txt`)
+    .output(
+        "output.txt", // filename
+        blob => Parse(FloatType, Utf8Decode(blob)) // fromBlob
+    )
+
+export default Template(my_source, custom_task);
--- 08_work_with_custom_tasks/02_custom_scenarios/03_optimize_a_custom_scenario/src/solution.ts
+++ 08_work_with_custom_tasks/02_custom_scenarios/03_optimize_a_custom_scenario/src/solution.ts
@@ -1,3 +1,22 @@
-import { Template } from "@elaraai/core"
+import { CustomScenarioBuilder, FloatType, Parse, Print, SourceBuilder, Template, Utf8Decode, Utf8Encode } from "@elaraai/core"
 
-export default Template();
+const my_source = new SourceBuilder("My Source")
+    .value({ value: 2 });
+
+const custom_task = new CustomScenarioBuilder("Quadratic")
+    .input(
+        "input.txt", // filename
+        my_source.outputStream(), // datastream
+        float => Utf8Encode(Print(float)), // toBlob
+    )
+    .shell(`awk '{x=$1; printf "%.14f\\n", x * (10 - x)}' input.txt > output.txt`)
+    .output(
+        "output.txt", // filename
+        blob => Parse(FloatType, Utf8Decode(blob)) // fromBlob
+    )
+    .objective(outputs => outputs['output.txt'])
+    .optimize("input.txt", { min: 0, max: 10 })
+    .optimizationMaxIterations(20)
+    .optimizationTrajectories(1);
+
+export default Template(my_source, custom_task);
